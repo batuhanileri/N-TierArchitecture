3777249ed698c1e7ca59c5fd3ad48e219bf3366b
batu_ <batu_@DESKTOP-AOMM71G>
2022-03-25 14:48:50 +0300

    UnitOfWork added

---
+++ NTierArchitecture.Data/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using NTierArchitecture.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.Data.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        
+        ICategoryDal Category { get; }
+        IAboutDal About { get; }
+        IContactDal Contact { get; }
+        ICommentDal Comment { get; }
+        IBlogDal Blog { get; }
+        IWriterDal Writer { get; }
+        Task CommitAsync();//implemente ettiğimiz zaman ef tarafında ki savechanges metodunu çağırıcak
+        void Commit();
+    }
+}
---
+++ NTierArchitecture.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using NTierArchitecture.Data.Abstract;
+using NTierArchitecture.Data.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Context _context;
+    
+        private EfCategoryDal _categoryRepository;
+
+        private EfAboutDal _aboutRepository;
+
+        private EfContactDal _contactRepository;
+
+        private EfCommentDal _commentRepository;
+
+        private EfBlogDal _blogRepository;
+
+        private EfWriterDal _writerRepository;
+
+        public IAboutDal Abouts => _aboutRepository = _aboutRepository ?? new EfAboutDal(_context);
+        public ICategoryDal Categories => _categoryRepository = _categoryRepository ?? new EfCategoryDal(_context);
+        public IContactDal Contacts => _contactRepository = _contactRepository ?? new EfContactDal(_context);
+        public ICommentDal Comment => _commentRepository= _commentRepository?? new EfCommentDal(_context);
+        public IBlogDal Blog => _blogRepository = _blogRepository ?? new EfBlogDal(_context);
+        public IWriterDal Writer => _writerRepository = _writerRepository ?? new EfWriterDal(_context);
+
+        public UnitOfWork(Context context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
